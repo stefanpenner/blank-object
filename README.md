e07ef6a54e8f6fe8c7c208ef8b11c090902aee6f
Kris Selden <kris.selden@gmail.com>
2015-09-04 10:24:41 -0700

    Cleanup
    Move comment to README.md, add repo to package.json, build index.js from ES6 (still checking it in for convenience since this project is tiny)

--- README.md
+++ README.md
@@ -9,3 +9,27 @@ var BlankObject = require('blank-object');
 
 var bo = new BlankObject();
 ```
+
+Every key is `undefined` but `"constructor" in blank` will return true.  This is designed for a presence check `map[key] !== undefined` since `in` is also slow like `hasOwnProperty`, `delete` and `Object.create`.
+
+```js
+function UNDEFINED() {}
+export default class Map {
+  constructor() {
+    this.store = new BlankObject();
+  }
+
+  has(key) {
+    return this.store[key] !== undefined;
+  }
+
+  get(key) {
+    let val = this.store[key];
+    return val === UNDEFINED ? undefined : val;
+  }
+
+  set(key, val) {
+    this.store[key] = val === undefined ? UNDEFINED : val;
+  }
+}
+```
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// This exists because `Object.create(null)` is absurdly slow compared
-// to `new EmptyObject()`. In either case, you want a null prototype
-// when you're treating the object instances as arbitrary dictionaries
-// and don't want your keys colliding with build-in methods on the
-// default object prototype.
-function BlankObject() {}
-BlankObject.prototype = Object.create(null, {
-  constructor: {
-    value: undefined,
-    enumerable: false,
-    writable: true
-  }
-});
-module.exports = BlankObject;
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  global.BlankObject = factory();
+}(this, function () { 'use strict';
+
+  function BlankObject() {}
+  BlankObject.prototype = Object.create(null, {
+    constructor: { value: undefined, enumerable: false, writable: true }
+  });
+
+  return BlankObject;
+
+}));
